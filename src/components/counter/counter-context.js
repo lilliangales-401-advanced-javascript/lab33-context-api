9cb2993da765a3d544ef551fe042cb4c28df2b3d
Lillian Gales <lilliangales@ymail.com>
2019-09-27 22:16:48 -0700

    code added from class review - need to go back and make changes

--- src/components/counter/counter-context.js
+++ src/components/counter/counter-context.js
@@ -1,46 +1,59 @@
 import React from 'react';
 
+// react context that gets imported by individual child components
 export const CounterContext = React.createContext();
 
-class CounterContext extends React.Component {
+// the content provider that wraps all children
+class CounterContextProvider extends React.Component {
   constructor(){
     super();
     this.state = {
       count: 0,
+      increment: this.increment,
+      decrement: this.decrement,
     }
   }
 
-  const styles = {
-    container: {
-      alignItems: 'center',
-      display: 'flex',
-      flexDirection: 'row',
-      justifyContent: 'space-evenly',
-      width: '40%',
-    },
-    counter: {
-      display: 'inline-block',
-      fontSize: '1em',
-      border: '2px solid #444',
-      borderRadius: '50%',
-      lineHeight: '5em',
-      textAlign: 'center',
-      width: '5em',
-    },
-    button: {
-      height: '3em',
-    },
-  };
+  increment = () => {
+    this.handleCounter(this.state.count + 1)
+  }
+
+  decrement = () => {
+    this.handleCounter(this.state.count - 1)
+  }
+
+  handleCounter = (count) => {
+    this.setState({count})
+  }
 
   render() {
+    // const styles = {
+    //   container: {
+    //     alignItems: 'center',
+    //     display: 'flex',
+    //     flexDirection: 'row',
+    //     justifyContent: 'space-evenly',
+    //     width: '40%',
+    //   },
+    //   counter: {
+    //     display: 'inline-block',
+    //     fontSize: '1em',
+    //     border: '2px solid #444',
+    //     borderRadius: '50%',
+    //     lineHeight: '5em',
+    //     textAlign: 'center',
+    //     width: '5em',
+    //   },
+    //   button: {
+    //     height: '3em',
+    //   },
+    // };
     return (
-      <div style={styles.container}>
-        <button style={styles.button} onClick={() => this.setState(this.state.count - 1)}> -</button>
-        <span style={styles.counter}>{count}</span>
-        <button style={styles.button} onClick={() => this.setState(this.state.count + 1)}> +</button>
-      </div>
+      <CounterContextProvider value={this.state}>
+        {this.props.children}
+      </CounterContextProvider>
     );
   }
 }
 
-export default CounterContext;
+export default CounterContextProvider;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './app.js';
+import { CounterContext } from "./components/counter/counter-context";
+
+// class Count extends React.Component {
+//
+//   static contextType = CounterContext;
+//
+//   render(){
+//     return(
+//       <div>
+//         {this.contextType.count}
+//       </div>
+//     )
+//   }
+// }
 
 class Main extends React.Component {
   render() {
